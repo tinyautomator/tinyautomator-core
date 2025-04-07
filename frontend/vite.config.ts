0b2012ea8e15ed8a005ad98b23bf1f23ca6fb073
Gabriel Terry <gabeterry51@gmail.com>
2025-04-07 02:03:05 -0400

    Add dashboard route with scaffolding

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter()],
+  plugins: [reactRouter(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": new URL("./src", import.meta.url).pathname,
+    },
+  },
 });
