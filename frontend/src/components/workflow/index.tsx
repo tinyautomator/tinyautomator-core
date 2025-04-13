0a21240a9f8c14be80226bc468c6fd0b6793f137
Turtleoo <gabterry51@gmail.com>
2025-04-13 04:50:09 -0400

    additional ui update

--- frontend/src/components/workflow/index.tsx
+++ frontend/src/components/workflow/index.tsx
@@ -1,10 +1,10 @@
 import "../../App.css";
-import { WorkflowBuilder } from "@/components/flow";
+import Flow from "@/components/flow";
 
 export default function () {
   return (
     <div className="h-full overflow-auto p-6 scrollbar-hidden">
-      <WorkflowBuilder></WorkflowBuilder>
+      <Flow></Flow>
     </div>
   );
 }
