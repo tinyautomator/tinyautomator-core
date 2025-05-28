7c71ed8aaa40ae65d8d047ec4d3078aece953267
Turtleoo <gjte222@uky.edu>
2025-05-28 13:58:40 -0400

    shadcn2

--- frontend/src/components/InspectorPanel/settings/index.tsx
+++ frontend/src/components/InspectorPanel/settings/index.tsx
@@ -1,6 +1,5 @@
 import { useFlowStore } from "@/components/Canvas/flowStore";
 import { EmailSettings } from "./email";
-import { ScheduleSettings } from "./schedule";
 
 export function SettingsTab() {
   const { getSelectedNode } = useFlowStore();
@@ -11,8 +10,6 @@ export function SettingsTab() {
   switch (selectedNode.data.nodeType as string) {
     case "send_email":
       return <EmailSettings />;
-    case "schedule":
-      return <ScheduleSettings />;
     default:
       return null;
   }
