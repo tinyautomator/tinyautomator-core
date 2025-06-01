b320a29c3ed3f3ab12d73a1ebcb0fcb8d4bbbe32
Gabriel Terry <131746323+Turtleoo@users.noreply.github.com>
2025-06-01 05:39:00 -0400

    spin (#83)

--- frontend/src/components/shared/GlobalSpinner.tsx
+++ frontend/src/components/shared/GlobalSpinner.tsx
@@ -3,14 +3,14 @@ import { Settings } from "lucide-react";
 function GlobalSpinner() {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/10 backdrop-blur-sm z-[9999]">
-      <div className="relative bottom-8 left-3.5 animate-[spin_3s_linear_infinite]">
-        <Settings className="w-8 h-8 text-gray-600 fill-white" />
+      <div className="relative bottom-8 left-3.5 animate-[spin_4s_linear_infinite]">
+        <Settings className="w-8 h-8 text-purple-600 fill-white" />
       </div>
-      <div className="animate-[spin_3s_linear_infinite_reverse]">
-        <Settings className="w-16 h-16 text-gray-600 fill-white" />
+      <div className="animate-[spin_4s_linear_infinite_reverse]">
+        <Settings className="w-16 h-16 text-gray-6bnv00 fill-white" />
       </div>
-      <div className="relative top-8 right-3.5 animate-[spin_3s_linear_infinite]">
-        <Settings className="w-8 h-8 text-gray-600 fill-white" />
+      <div className="relative top-8 right-3.5 animate-[spin_4s_linear_infinite]">
+        <Settings className="w-8 h-8 text-amber-600 fill-white" />
       </div>
     </div>
   );
