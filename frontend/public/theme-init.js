002aba8a9d21a65cf6d2e0e01121b832cd727335
Paul Vinueza <paulvinueza30@gmail.com>
2025-06-03 15:16:06 -0400

    public script for inti theme

---
+++ frontend/public/theme-init.js
@@ -0,0 +1,17 @@
+// Get initial theme from cookie
+const getInitialTheme = () => {
+  try {
+    const theme =
+      document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("theme="))
+        ?.split("=")[1] || "light";
+    return theme;
+  } catch (e) {
+    return "light";
+  }
+};
+
+const theme = getInitialTheme();
+document.documentElement.classList.add(theme);
+document.documentElement.classList.remove(theme === "dark" ? "light" : "dark");
