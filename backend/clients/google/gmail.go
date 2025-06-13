3f004075676221919b3d164ba7faad53ec541e61
Paul Vinueza <paulvinueza30@gmail.com>
2025-06-13 16:52:07 -0400

    refactor gmail client so init token in one place (#106)
    * refactor gmail client so init token in one place
    
    * refactor gmail client so init token in one place

--- backend/clients/google/gmail.go
+++ backend/clients/google/gmail.go
@@ -9,25 +9,35 @@ import (
 	"google.golang.org/api/option"
 )
 
+type GmailClient struct {
+	service *gmail.Service
+}
+
 type GmailToken struct {
 	AccessToken  string `json:"access_token"`
 	RefreshToken string `json:"refresh_token"`
 	Expiry       string `json:"expiry"`
 }
 
-func GetUserEmail(
+func InitGmailClient(
 	ctx context.Context,
 	token *oauth2.Token,
 	oauthConfig *oauth2.Config,
-) (string, error) {
+) (*GmailClient, error) {
 	tokenSource := oauthConfig.TokenSource(ctx, token)
 
 	service, err := gmail.NewService(ctx, option.WithTokenSource(tokenSource))
 	if err != nil {
-		return "", fmt.Errorf("unable to init gmail service: %w", err)
+		return nil, fmt.Errorf("unable to init gmail service: %w", err)
 	}
 
-	profile, err := service.Users.GetProfile("me").Do()
+	return &GmailClient{service: service}, nil
+}
+
+func (c *GmailClient) GetUserEmail(
+	ctx context.Context,
+) (string, error) {
+	profile, err := c.service.Users.GetProfile("me").Do()
 	if err != nil {
 		return "", fmt.Errorf("unable to get the user's profile: %w", err)
 	}
@@ -35,24 +45,15 @@ func GetUserEmail(
 	return profile.EmailAddress, nil
 }
 
-func SendRawEmail(
+func (c *GmailClient) SendRawEmail(
 	ctx context.Context,
-	token *oauth2.Token,
-	oauthConfig *oauth2.Config,
 	encodedMIME string,
 ) error {
-	tokenSource := oauthConfig.TokenSource(ctx, token)
-
-	gmailService, err := gmail.NewService(ctx, option.WithTokenSource(tokenSource))
-	if err != nil {
-		return fmt.Errorf("failed to create Gmail client: %w", err)
-	}
-
 	msg := &gmail.Message{
 		Raw: encodedMIME,
 	}
 
-	_, err = gmailService.Users.Messages.Send("me", msg).Do()
+	_, err := c.service.Users.Messages.Send("me", msg).Do()
 	if err != nil {
 		return fmt.Errorf("failed to send Gmail message: %w", err)
 	}
